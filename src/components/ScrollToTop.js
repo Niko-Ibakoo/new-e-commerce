4f3849f25a3cc255c4a14668ed520345295afd67
Niko <germano.niko01@gmail.com>
2023-03-30 16:53:36 -0400

    added pic

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-
+import { useLocation } from "react-router-dom"; // collection of all endpoint in the adress
+//uselocation
 export default function ScroolToTop() {
-  const { pathname } = useLocation();
+  const {pathname} = useLocation(); //destructuring the whole page to get only the pathname
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
+
 }
